7d07e00736f03d6d029dc143f8cf68db8bce6363
Jason Yatfai Zhang <jzhang5137@gmail.com>
2018-07-14 20:16:44 -0700

    chose source sans google font

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,11 +11,14 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
 body {
   margin: 0 !important;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Source Sans Pro', sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
